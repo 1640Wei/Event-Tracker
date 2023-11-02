34f230fd91ca09cab4645e34a67d609c90593328
Ching-Wei Lai <103228835+1640Wei@users.noreply.github.com>
2023-11-02 16:12:47 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,70 +2,93 @@ import requests
 from tkinter import *
 from PIL import Image, ImageTk
 from io import BytesIO
+import socket
+
+
+def set_dns(server_ip):
+   # 使用 socket 设置 DNS 服务器
+   socket.create_connection((server_ip, 53), timeout=10)
+   socket.setdefaulttimeout(10)
+
+
+
 
 def get_nasa_events(start_date, end_date):
-    api_url = f"https://eonet.sci.gsfc.nasa.gov/api/v3/events?start_date={start_date}&end_date={end_date}"
+    dns_server = "8.8.8.8"
+    set_dns(dns_server)
+
+
+    api_url = f"https://eonet.gsfc.nasa.gov/api/v3/events?start={start_date}&end={end_date}"
     response = requests.get(api_url)
+
 
     if response.status_code == 200:
         data = response.json()
         events = data.get("events", [])
 
+
+        if not events:
+            print("沒有可用數據。")
+            return
+
+
         for event in events:
             print("事件名稱:", event.get("title"))
-            print("位置:", event.get("geometry", {}).get("coordinates"))
-            print("發生時間:", event.get("geometries", [{}])[0].get("date"))
-            print("類型:", event.get("categories", [])[0].get("title"))
+            coordinates = event.get("geometry", [{}])[0].get("coordinates")
+            date = event.get("geometries", [{}])[0].get("date")
+            print("位置:", coordinates if coordinates else "未提供")
+            print("發生時間:", date if date else "未提供")
+            print("類型:", event.get("categories", [{}])[0].get("title"))
             print("----------------------------------")
-
-            # 在這裡你可以處理圖片的顯示，例如：
-            image_url = event.get("links", {}).get("image", "")
-            display_image(image_url)
-
     else:
         print("無法取得數據，錯誤碼:", response.status_code)
 
+
+
+
+
+
+
+
 def display_image(image_url):
-    if image_url:
-        response = requests.get(image_url)
-        img_data = response.content
-        img = Image.open(BytesIO(img_data))
+   response = requests.get(image_url)
+   img_data = response.content
+   img = Image.open(BytesIO(img_data))
+   img.show()
 
-        # 使用 tkinter 來顯示圖片
-        img = img.resize((300, 300))  # 調整圖片大小
-        photo = ImageTk.PhotoImage(img)
-        image_label.config(image=photo)
-        image_label.image = photo
-        image_label.pack()
 
-# 提交按鈕的事件處理函數
+
+
 def on_submit():
-    start_date = start_date_entry.get()
-    end_date = end_date_entry.get()
+   start_date = start_date_entry.get()
+   end_date = end_date_entry.get()
 
-    # 在這裡你可以使用日期區間去呼叫 get_nasa_events 函數
-    get_nasa_events(start_date, end_date)
+
+   get_nasa_events(start_date, end_date)
+
+
+
 
 # 創建主窗口
 root = Tk()
 root.title("NASA Events Tracker")
+
 
 # 創建日期輸入框
 Label(root, text="Start Date (YYYY-MM-DD):").pack()
 start_date_entry = Entry(root)
 start_date_entry.pack()
 
+
 Label(root, text="End Date (YYYY-MM-DD):").pack()
 end_date_entry = Entry(root)
 end_date_entry.pack()
+
 
 # 創建提交按鈕
 submit_button = Button(root, text="Submit", command=on_submit)
 submit_button.pack()
 
-# 創建顯示圖片的標籤
-image_label = Label(root)
-image_label.pack()
 
 # 啟動主循環
 root.mainloop()
